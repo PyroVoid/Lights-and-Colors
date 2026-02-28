a8cd123035f23020f6ea2d86be65a345bcafa710
Benjamin Hewitt <bxh2408@rit.edu>
2026-02-28 16:06:53 -0500

    start of mapper

--- AutoMapper.py
+++ AutoMapper.py
@@ -1,6 +1,6 @@
 from TreeSimulation import Tree
 import Controller
-from LightDiffAlgoTmp import get_diffs
+from LightDiffAlgoTmp import get_diffs, take_pic
 
 def map():
     coords = []
@@ -8,6 +8,13 @@ def map():
     for i in range(Tree.LIGHTS):
         controller.set_color(i, (0, 0, 0))
     for i in range(Tree.LIGHTS):
+        take_pic()
         controller.set_color(i, (255, 255, 255))
+        take_pic()
         coords.append(get_diffs())
         controller.set_color(i, (0, 0, 0))
+    return coords
+
+if __name__ == "__main__":
+    coords = map()
+    print(coords)
--- LightDiffAlgoTmp.py
+++ LightDiffAlgoTmp.py
@@ -2,3 +2,6 @@
 
 def get_diffs():
     return {(120, 300), (200, 150)}
+
+def take_pic():
+    pass
