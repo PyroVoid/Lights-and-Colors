90a237b211b1328f2b2bd92a75aecce838ecd028
Benjamin Hewitt <bxh2408@rit.edu>
2026-02-28 19:44:36 -0500

    start of mapper

--- AutoMapper.py
+++ AutoMapper.py
@@ -4,31 +4,33 @@ from LightDiffAlgo import algorithm, take_photo
 import cv2
 import platform
 
-if platform.system() == "Windows":
-    cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
-else:
-    cap = cv2.VideoCapture(1)
-controller = Controller.Controller()
+class Automapper:
+    def __init__(self, cap):
+        if platform.system() == "Windows":
+            self.cap = cv2.VideoCapture(cap, cv2.CAP_DSHOW)
+        else:
+            self.cap = cv2.VideoCapture(cap)
+        self.controller = Controller.Controller()
 
+    def find_led(self, i):
+        leds = set()
+        while len(leds) != 1:
+            self.controller.set_all((0, 0, 0))
+            first = take_photo(self.cap)
+            self.controller.set_color(i, (255, 255, 255))
+            second = take_photo(self.cap)
+            leds = algorithm(first, second)
+        return leds[0]
 
-def find_led(i):
-    leds = set()
-    while len(leds) != 1:
-        controller.set_all((0, 0, 0))
-        first = take_photo(cap)
-        controller.set_color(i, (255, 255, 255))
-        second = take_photo(cap)
-        leds = algorithm(first, second)
-    return leds[0]
-
-def map():
-    coords = []
-    for i in range(Tree.LIGHTS):
-        coords.append(find_led(i))
-    cap.release()
-    return coords
+    def map(self):
+        coords = []
+        for i in range(Tree.LIGHTS):
+            coords.append(self.find_led(i))
+        self.cap.release()
+        return coords
 
 if __name__ == "__main__":
-    coords = map()
+    mapper = Automapper(1)
+    coords = mapper.map()
     print(coords)
     print(len(coords))
